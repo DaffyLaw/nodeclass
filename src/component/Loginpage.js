5938ab389167e3f8c652529fad1d7716a702f260
huanliu <lh19970701@gmail.com>
2024-03-13 13:47:44 +1100

    Initial Commit

---
+++ src/component/Loginpage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Loginpage(){
+return(
+    <div>
+        <h1>login</h1>
+        账户：<input className='input_box' type="text"></input>
+        <br />
+        密码：<input className='input_box' type='password'></input>
+    </div>
+)
+}
+export default Loginpage
